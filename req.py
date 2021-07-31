92c7988ba5f325269e994b4b4a0cdba328403b50
Anatoly Scherbakov <altaisoft@gmail.com>
2021-07-31 20:41:23 +0700

    Working on context3.json

---
+++ req.py
@@ -0,0 +1,17 @@
+{
+  'contentType': 'application/ld+json',
+  'contextUrl': None,
+  'documentUrl': 'https://prefix.cc/context',
+  'document': {
+    '@context': {
+      'madsrdf': 'http://www.loc.gov/mads/rdf/v1#',
+      'bflc': 'http://id.loc.gov/ontologies/bflc/',
+      'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      'foaf': 'http://xmlns.com/foaf/0.1/',
+      'yago': 'http://yago-knowledge.org/resource/',
+      'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+      'dbo': 'http://dbpedia.org/ontology/',
+      'ex': 'http://example.org/',
+    }
+  }
+}
